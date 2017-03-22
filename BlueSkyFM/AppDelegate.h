ecce32f25c15511143d6539869295af1ac1ad2e2
ddEfforts <m18210681011@163.com>
2017-03-22 16:42:09 +0800

    init

---
+++ BlueSkyFM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlueSkyFM
+//
+//  Created by 张东东 on 2017/3/22.
+//  Copyright © 2017年 张东东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlueSkyFM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlueSkyFM
+//
+//  Created by 张东东 on 2017/3/22.
+//  Copyright © 2017年 张东东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
